37fca3a9706c740773c6b0b47de69085dd527685
Ayushi Pandey <ayushipandey75@gmail.com>
2023-04-29 17:38:46 +0530

    feat: added colors in the signup form

--- src/components/Auth/AuthSignup.jsx
+++ src/components/Auth/AuthSignup.jsx
@@ -1,5 +1,5 @@
 import "./Auth.css";
-import { useAuth , useAlert} from "../../context";
+import { useAuth, useAlert } from "../../context";
 import {
   validateName,
   validateEmail,
@@ -8,83 +8,106 @@ import {
 } from "../../utils";
 
 import { signupHandler } from "../../services";
+import { useState } from "react";
 
 let isNameValid,
   isNumberValid,
   isEmailValid,
   isPasswordValid,
-  isConfirmPasswordValid;
+  isConfirmPasswordValid,
+  passwordEvent,
+  confirmPasswordEvent;
 
 export const AuthSignup = () => {
-  const { number ,username, email, password, confirmPassword, authDispatch } =
+  const { number, username, email, password, confirmPassword, authDispatch } =
     useAuth();
 
   // console.log({username, email, password, number, confirmPassword, authDispatch});
 
-  const { setAlert } = useAlert();  
+  const { setAlert } = useAlert();
+
+  const [numberValid, setNumberValid] = useState("");
+  const [nameValid, setNameValid] = useState("");
+  const [emailValid, setEmailValid] = useState("");
+  const [passwordValid, setPasswordValid] = useState("");
+  const [confirmPassswordValid, setConfirmPasswordValid] = useState("");
 
   const handleNumberChange = (event) => {
     isNumberValid = validateNumber(event.target.value);
     if (isNumberValid) {
-      console.log("Valid Input")
+      setNumberValid("Valid Number");
       authDispatch({
         type: "NUMBER",
         payload: event.target.value,
       });
+    } else if (event.target.value === "") {
+      setNumberValid("Enter Number");
     } else {
-      console.log("Invalid Number");
+      setNumberValid("Invalid Number");
     }
   };
 
   const handleNameChange = (event) => {
     isNameValid = validateName(event.target.value);
     if (isNameValid) {
-      console.log("Valid Input")
+      setNameValid("Valid Name");
       authDispatch({
         type: "NAME",
         payload: event.target.value,
       });
+    } else if (event.target.value === "") {
+      setNameValid("Enter Name");
     } else {
-      console.log("Invalid Name");
+      setNameValid("Invalid Name");
     }
   };
 
   const handleEmailChange = (event) => {
     isEmailValid = validateEmail(event.target.value);
     if (isEmailValid) {
-      console.log("Valid Input")
+      setEmailValid("Valid Email");
       authDispatch({
         type: "EMAIL",
         payload: event.target.value,
       });
+    } else if (event.target.value === "") {
+      setEmailValid("Enter Email");
     } else {
-      console.log("Invalid Email");
+      setEmailValid("Invalid Email");
     }
   };
 
   const handlePasswordChange = (event) => {
+    passwordEvent = event.target.value;
     isPasswordValid = validatePassword(event.target.value);
     if (isPasswordValid) {
-      console.log("Valid Input")
+      setPasswordValid("Valid Password");
       authDispatch({
         type: "PASSWORD",
         payload: event.target.value,
       });
+    } else if (event.target.value === "") {
+      setPasswordValid("Enter Password");
     } else {
-      console.log("Invalid Password");
+      setPasswordValid(
+        "Invalid Password, password should contain atleast 8 characters including 1 capital letter , 1 special character and 1 number"
+      );
     }
   };
 
   const handleConfirmPasswordChange = (event) => {
+    confirmPasswordEvent = event.target.value;
     isConfirmPasswordValid = validatePassword(event.target.value);
-    if (isConfirmPasswordValid) {
-      console.log("Valid Input")
+    if (isConfirmPasswordValid && passwordEvent === confirmPasswordEvent) {
+      setConfirmPasswordValid("Password Matched");
       authDispatch({
         type: "CONFIRM_PASSWORD",
         payload: event.target.value,
       });
+    } else if (event.target.value === "") {
+      setConfirmPasswordValid("Enter Password again");
     } else {
-      console.log("Invalid Password");
+      setConfirmPasswordValid("Password did not match");
     }
   };
 
@@ -97,18 +120,18 @@ export const AuthSignup = () => {
       isPasswordValid &&
       isConfirmPasswordValid
     ) {
-      signupHandler( number,username, email, password);
+      signupHandler(number, username, email, password);
     }
     authDispatch({
-      type: "CLEAR_USER_DATA"
-    })
-    setTimeout (()=> {
+      type: "CLEAR_USER_DATA",
+    });
+    setTimeout(() => {
       setAlert({
         open: true,
         message: `Sign-in Successful !!! Please Login now.`,
-        type: "success"
-      })
-    }, 4000)    
+        type: "success",
+      });
+    }, 4000);
   };
 
   return (
@@ -127,6 +150,14 @@ export const AuthSignup = () => {
             required
             onChange={handleNumberChange}
           />
+          {/* <span className="sinup-span">{numberEvent === "" ? "Enter number" : isNumberValid ? "Valid input" : "Invalid Input"}</span> */}
+          <p
+            className={`signup-span ${
+              isNumberValid ? "singup-success" : "singup-wrong"
+            }`}
+          >
+            {numberValid}
+          </p>
         </div>
         <div className="d-flex direction-column lb-in-container">
           <label className="auth-label">
@@ -139,6 +170,13 @@ export const AuthSignup = () => {
             required
             onChange={handleNameChange}
           />
+          <p
+            className={`signup-span ${
+              isNameValid ? "singup-success" : "singup-wrong"
+            }`}
+          >
+            {nameValid}
+          </p>
         </div>
         <div className="d-flex direction-column lb-in-container">
           <label className="auth-label">
@@ -152,6 +190,13 @@ export const AuthSignup = () => {
             required
             onChange={handleEmailChange}
           />
+          <p
+            className={`signup-span ${
+              isEmailValid ? "singup-success" : "singup-wrong"
+            }`}
+          >
+            {emailValid}
+          </p>
         </div>
         <div className="d-flex direction-column lb-in-container">
           <label className="auth-label">
@@ -165,6 +210,13 @@ export const AuthSignup = () => {
             required
             onChange={handlePasswordChange}
           />
+          <p
+            className={`signup-span ${
+              isPasswordValid ? "singup-success" : "singup-wrong"
+            }`}
+          >
+            {passwordValid}
+          </p>
         </div>
         <div className="d-flex direction-column lb-in-container">
           <label className="auth-label">
@@ -178,6 +230,13 @@ export const AuthSignup = () => {
             required
             onChange={handleConfirmPasswordChange}
           />
+          <p
+            className={`signup-span ${
+              isConfirmPasswordValid ? "singup-success" : "singup-wrong"
+            }`}
+          >
+            {confirmPassswordValid}
+          </p>
         </div>
         <div>
           <button className="button btn-primary btn-login cursor">
